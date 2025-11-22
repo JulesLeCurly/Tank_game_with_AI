2d33efd97a834e053bf9abb45686102c71ba98da
JulesLeCurly <jules.depret1@gmail.com>
2025-11-23 00:09:39 +0100

    Fixing color

--- Function/core/Tank.py
+++ Function/core/Tank.py
@@ -2,7 +2,7 @@ import pygame
 import math
 from PIL import Image
 import numpy as np
-cocotank={"red":(255,0,0), "blue":(0,0,255)}
+cocotank={"red":(100,0,0), "blue":(0,0,100)}
 class Tank:
     def __init__(self, width, height, x, color_tank):
         self.x = x
