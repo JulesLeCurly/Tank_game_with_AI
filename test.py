551e67d1dda96d42f6f55b1bb6157f3b07761a8b
oneeyes18-coder <one.eyes18@gmail.com>
2025-11-22 23:30:02 +0100

    add colore tanks in phifier

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
-import Function.core.Terrain as Terrain
-import time
+from PIL import Image
 
-t = Terrain.Terrain(1200, 650, 500)
+# -------- CONFIG --------
+input_path = "Images/Tank.png"          # Image d'origine
+output_path = "Tank_red.png"      # Image transformée en rouge
+# ------------------------
 
-for i in range(2000):
-    t.generate_terrain()
-    time.sleep(1)
+# Charger l'image
+img = Image.open(input_path).convert("RGBA")
+pixels = img.load()
+
+for y in range(img.height):
+    for x in range(img.width):
+        r, g, b, a = pixels[x, y]
+
+        # Détection d'un pixel vert (inclut les dégradés)
+        if g > r * 1.2 and g > b * 1.2 and g > 30:
+
+            # Transformer le vert en rouge en gardant l'intensité du vert
+            new_r = g                     # rouge basé sur la luminosité du vert
+            new_g = int(g * 0.3)          # réduit pour ne pas garder du vert
+            new_b = int(b * 0.3)          # légèrement réduit
+
+            pixels[x, y] = (new_r, new_g, new_b, a)
+
+# Sauvegarde
+img.save(output_path)
+print("Image générée :", output_path)
